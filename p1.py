674c1d72d163dd0c72bb3ac01f3f0a24ed1884d7
pedroFabriz <pedrofabriz2000@gmail.com>
2022-04-16 15:48:41 -0300

    Part 3

--- p1.py
+++ p1.py
@@ -13,3 +13,25 @@ output = input[0]*weights[0] + input[1]*weights[1] + input[2]*weights[2] + bias
 # This is basically what a neuron does in the neural network, we just have to put together a bunch of them.
 # --------------------------------------------------------------------------------- #
 
+# creating a loop for a layer calculation
+
+inputs = [3,2,1,3]
+weights = [[1,2,3,4],
+            [0,3.5,2,1],
+            [2.3,1.2,3,4]]
+
+biases = [2,3,4]
+
+layer_output = []
+
+for w, b in  zip(weights, biases):
+    output = 0
+    for i, w_ in zip(inputs,w):
+        output += i*w_
+    output += b
+    layer_output.append(output)
+
+print(layer_output)
+
+# --------------------------------------------------
+
