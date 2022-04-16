f713c577fe900d8ae0d99fa409cc7a3724e90b8a
pedroFabriz <pedrofabriz2000@gmail.com>
2022-04-16 15:10:49 -0300

    Part 1

---
+++ p1.py
@@ -0,0 +1,15 @@
+# common neuron : output = input*weight + bias
+
+input = [1,2,3]
+output = []
+bias = 3
+weights = [0.2,0.3,0.4]
+
+# neuron:
+
+output = input[0]*weights[0] + input[1]*weights[1] + input[2]*weights[2] + bias
+#print(bias)
+
+# This is basically what a neuron does in the neural network, we just have to put together a bunch of them.
+# --------------------------------------------------------------------------------- #
+
